66398ecc8549cbb605249ae1ce4429bf1740b275
Comfort Ohajunwa <comfortohajunwa1@gmail.com>
2025-04-06 16:27:57 -0400

    Add logic for loading and modifying dataset (flattening & masking Python methods)

--- fine_tune.py
+++ fine_tune.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import re
 import sys
 
 from transformers import T5ForConditionalGeneration, AutoModelForSeq2SeqLM
@@ -10,17 +12,44 @@ from datasets import Dataset
 from datasets import load_dataset
 
 
-def modify_dataset(dataset):
-    """Modify dataset by flattening methods and masking if conditions"""
-    pass
+def flatten(example):
+  method = example['cleaned_method']
+
+  flattened_method = method.replace('    ', '<TAB>').replace('\n', '')
+  example['cleaned_method'] = flattened_method
+  return example
+
+def mask(example):
+  if_statement_pattern = r"if\s+.*"
+
+  masked_method = re.sub(if_statement_pattern, "<IF-STMT>",
+                         example['cleaned_method'], 1)
+  example['masked_method'] = masked_method
+
+  return example
 
 def tokenization(dataset):
     """Tokenize dataset using pre-trained tokenizer"""
     pass
 
-def load_model_and_tokenizer():
-    """Load pre-trained Code-T5 model and tokenizer"""
-    pass
+def run(dataset):
+    """Pipeline for loading pre-trained Code-T5 model and tokenizer, modifying dataset, and fine-tuning"""
+    print("Loading model and tokenizer")
+
+    model_checkpoint = "Salesforce/codet5-small"
+
+    model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
+
+    tokenizer = RobertaTokenizer.from_pretrained(model_checkpoint)
+
+    tokenizer.add_tokens(["<IF-STMT>", "<TAB>"])
+
+    model.resize_token_embeddings(len(tokenizer))
+
+
+    print("Modifying dataset by flattening methods and masking if conditions")    
+    dataset = dataset.map(flatten).map(mask)
+    print(dataset)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -34,5 +63,8 @@ if __name__ == '__main__':
         print("Missing file path(s) for training, validation, and/or testing dataset")
         sys.exit()
     
-
+    print("Loading dataset for fine-tuning CodeT5")
     dataset = load_dataset("csv", data_files = {"train": args.train_file, "validation": args.validation_file, "test": args.test_file})
+
+    print(dataset)
+    run(dataset)
